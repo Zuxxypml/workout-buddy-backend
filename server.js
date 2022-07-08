8218acdb9065890e73a37d10482bc75544b483df
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-07-08 09:26:40 +0100

    API 100%

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const workoutRoutes = require("./routes/workouts");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT;
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://stellular-banoffee-219c09.netlify.app/"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Middleware
 app.use(express.json());
