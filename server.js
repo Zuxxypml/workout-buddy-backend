2c7c4c1de98aff288c42f1e673ef0a1ca9ded9ef
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-07-08 10:28:21 +0100

    API 100%

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
